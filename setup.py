adab864a21da4fffebc6c4259e086d90aba8ff83
InCogNiTo124 <msmetko@msmetko.xyz>
2021-10-09 14:47:36 +0200

    Fix stub naming and add build for master push

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ import setuptools
 project_dir = Path(__file__).parent
 
 setuptools.setup(
-    name="stubs",
+    name="knarrow",
     version="0.0.2",
-    description="Shoot a stubs to the knee",
+    description="Shoot a knarrow to the knee",
     # Use UTF-8 encoding for README even on Windows by using the encoding argument.
     long_description=project_dir.joinpath("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     keywords=["python"],
     author="Marijan Smetko",
+    author_email="msmetko@msmetko.xyz",
     url="https://github.com/InCogNiTo124/knarrow",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
@@ -28,7 +29,7 @@ setuptools.setup(
     include_package_data=True,
     # This file is required to inform mypy that inline type hints are used.
     #   See: https://mypy.readthedocs.io/en/stable/installed_packages.html
-    package_data={"stubs": ["py.typed"]},
+    package_data={"knarrow": ["py.typed"]},
     # This is a trick to avoid duplicating dependencies between both setup.py and
     # requirements.txt.
     # requirements.txt must be included in MANIFEST.in for this to work.
