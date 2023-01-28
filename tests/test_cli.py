c5dc95f9221b4e3b26b96611fcfd305872bfd863
InCogNiTo124 <msmetko@msmetko.xyz>
2023-01-29 00:36:04 +0100

    Add more changes

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,16 @@
 from typer.testing import CliRunner
 
-from knarrow.cli import app
 from knarrow.__about__ import __version__
+from knarrow.cli import app
 
 runner = CliRunner()
 
+
 def test_version():
-    result = runner.invoke(app, ['--version'])
+    result = runner.invoke(app, ["--version"])
     assert result.exit_code == 0
     assert str(__version__) in result.stdout
+
 
 def test_manual_input():
     # FIXME input does not seem to be working ??
@@ -16,13 +18,15 @@ def test_manual_input():
     assert result.exit_code == 0
     assert result.stdout == "stdin 3"
 
+
 def test_method():
     # FIXME input does not seem to be working ??
-    result = runner.invoke(app, ['--method', 'c_method'], input=b"1\n2\n3\n5\n")
+    result = runner.invoke(app, ["--method", "c_method"], input=b"1\n2\n3\n5\n")
     assert result.exit_code == 0
     assert result.stdout == "stdin 3"
 
+
 def test_sort_input_file():
-    result = runner.invoke(app, ['--sort', '--output', 'index', '--smoothing', '0.01', 'tests/cli/shuf_delim.txt'])
+    result = runner.invoke(app, ["--sort", "--output", "index", "--smoothing", "0.01", "tests/cli/shuf_delim.txt"])
     assert result.exit_code == 0
     assert result.stdout.strip() == "shuf_delim.txt 14"
